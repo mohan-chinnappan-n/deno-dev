9c8799d8d28771ac9592bd1dd98c21ba0c623633
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2021-08-27 06:11:38 -0400

    init

--- fetch.ts
+++ fetch.ts
@@ -1,5 +1,9 @@
 const url = Deno.args[0];
 // example run: deno run --allow-net fetch.ts  https://mohan-chinnappan-n2.github.io/media-app.html
+// from github
+//  deno run --allow-net https://raw.githubusercontent.com/mohan-chinnappan-n/deno-dev/master/fetch.ts  https://mohan-chinnappan-n2.github.io/media-app.html
+
+
 console.log(url);
 const res = await fetch(url);
 
--- ws.ts
+++ ws.ts
@@ -1,4 +1,9 @@
-const listener = Deno.listen({ port: 8000 });
+const port = Deno.args[0];
+
+const listener = Deno.listen({ port  });
+
+// deno run --allow-net https://raw.githubusercontent.com/mohan-chinnappan-n/deno-dev/master/ws.ts  8000
+
 console.log("http://localhost:8000/");
 for await (const conn of listener) {
   (async () => {
